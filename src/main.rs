2de606b41903a81104c83b21bef1a0af5639a77d
Tyler Pollom <tylerpollom@protonmail.com>
2021-01-03 13:07:25 -0700

    Comment general info and basic plan

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+/* A minimal assembler for the Hack computer from Nand2Tetris.
+    Takes an input file "abc.asm" in the symbolic Hack machine language and writes the corrosponding binary
+    to "abc.hack"
+    Implementation details and course: 
+    https://www.coursera.org/learn/build-a-computer?s
+    https://www.nand2tetris.org/project06 
+    
+   Project to be built in sections:
+    1. Basic assembler that can translate files that do not use symbols
+        - Parser module that breaks input file into pieces
+        - Code module that translates pieces into "binary instructoins"
+    2. Add the ability to handle symbols like variable names and jump labels
+        - SymbolTable module that tracks symbols and labels with their memory addresses.
+*/
 fn main() {
     println!("Hello, world!");
 }
