18ad5713d575614fdebc2bb055a2e649838ca31e
Tyler Pollom <tylerpollom@protonmail.com>
2021-01-03 18:09:42 -0700

    Read and write ability; ignore course-supplied material.
    Add the ability to read the contents of an input file, and the ability
    to write content to another file of the same base name as the input
    file.
    Calling "hack_assembler abc.asm" will now create a file abc.hack with the
    same content as abc.asm
    
    Also update .gitignore to ignore content supplied by the Coursera course
    as it is not mine to share/upload/etc.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::{fs::File, io::Write, path::PathBuf};
+
 /* A minimal assembler for the Hack computer from Nand2Tetris.
     Takes an input file "abc.asm" in the symbolic Hack machine language and writes the corrosponding binary
     to "abc.hack"
@@ -20,7 +22,38 @@ struct Cli {
     path: std::path::PathBuf,
 }
 
+/// Return the contents of the supplied file as a String, or panic.
+fn get_file_contents(asm_file: &PathBuf) -> String {
+    match std::fs::read_to_string(asm_file) {
+        Ok(r) => r,
+        Err(e) => panic!("Couldn't read from file! Error: {}", e),
+    }
+}
+
+/// Write the supplied string to the file "filename".
+// filename can be any string
+// to_write can by any type that implements the lines() function.
+fn write_binary_to_file(filename: String, to_write: String) -> std::io::Result<()> {
+    let mut output_file = File::create(filename)?;
+    for line in to_write.lines() {
+        let properly_formatted_line = line.to_owned() + "\n";
+        output_file.write(properly_formatted_line.as_bytes())?;
+    }
+    Ok(())
+}
+
 fn main() {
     let args = Cli::from_args();
-    println!("{:?}", &args);
+    let contents = get_file_contents(&args.path);
+
+    let mut output_filename: String = match args.path.file_stem() {
+        Some(filename) => String::from(filename.to_str().unwrap()),
+        None => panic!("We tried to get the filename from user's input, but one didn't exist!"),
+    };
+    output_filename.push_str(".hack");
+
+    match write_binary_to_file(output_filename, contents) {
+        Ok(_) => (),
+        Err(e) => panic!("Failed to write output to file: {}", e),
+    };
 }
