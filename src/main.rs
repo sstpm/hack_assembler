db1f6b04fc0c4367de0a6bfaed706697e98a0889
Tyler Pollom <tylerpollom@protonmail.com>
2021-01-04 23:42:29 -0700

    Write the translator; output binary to file.
    Translate the parsed content to the "binary" representation, then write
    that output to "filename-assembled.hack". Translator uses hashmaps with
    Option<String> as the key (the instruction parts) and a string of 1s and
    0s as the value.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
-use std::{fs::File, io::Write, path::PathBuf, todo};
+use std::{collections::HashMap, fs::File, io::Write, path::PathBuf, todo};
+use structopt::StructOpt;
 
 /* A minimal assembler for the Hack computer from Nand2Tetris.
     Takes an input file "abc.asm" in the symbolic Hack machine language and writes the corrosponding binary
@@ -31,7 +32,6 @@ use std::{fs::File, io::Write, path::PathBuf, todo};
     2. Add the ability to handle symbols like variable names and jump labels
         - SymbolTable module that tracks symbols and labels with their memory addresses.
 */
-use structopt::StructOpt;
 #[derive(Debug, StructOpt)]
 // StructOpt crate for command line argument parsing (only the path of input file for now).
 struct Cli {
@@ -39,14 +39,14 @@ struct Cli {
     path: std::path::PathBuf,
 }
 
-#[derive(Debug, PartialEq)]
+#[derive(Debug, PartialEq, Eq, Hash)]
 enum CommandKind {
     ACommand,
     CCommand,
     LCommand,
     ICommand, // An invalid command, returned upon encountering a line that is not an instruction. Ignored completely.
 }
-#[derive(Debug)]
+#[derive(Debug, PartialEq, Eq, Hash)]
 struct ParsedLine {
     // The "parts" of an instruction. CommandKind is either A, C, or L(abel).
     // Depending on the CommandKind, Dest, Comp, and Jump are either Some(value) or None
@@ -63,7 +63,6 @@ struct ParsedLine {
     comp: Option<String>,
     jump: Option<String>,
 }
-
 
 fn parse_line(line: String) -> ParsedLine {
     // Assumes the line has been preprocessed already. That means all comments and whitespace have been removed and the
@@ -98,7 +97,13 @@ fn parse_line(line: String) -> ParsedLine {
                         .chars()
                         .position(|x| x == '+' || x == '&' || x == '|')
                         .unwrap();
-                    &temp_comp.push_str( line.chars().skip(pos_of_op).take(2).collect::<String>().as_str());
+                    &temp_comp.push_str(
+                        line.chars()
+                            .skip(pos_of_op)
+                            .take(2)
+                            .collect::<String>()
+                            .as_str(),
+                    );
                 } else {
                     // There's no destination but we still have some operation going on, eg D+M
                     // In this case, we won't have handled the first part of the comparison yet.
@@ -106,7 +111,13 @@ fn parse_line(line: String) -> ParsedLine {
                         .chars()
                         .position(|x| x == '+' || x == '&' || x == '|')
                         .unwrap();
-                    &temp_comp.push_str( line.chars().skip(pos_of_op - 1).take(3).collect::<String>().as_str());
+                    &temp_comp.push_str(
+                        line.chars()
+                            .skip(pos_of_op - 1)
+                            .take(3)
+                            .collect::<String>()
+                            .as_str(),
+                    );
                 }
             } else if char == '-' || char == '!' {
                 // -X, !X, D=-X, D=!X, D=M-X, M-X
@@ -121,9 +132,21 @@ fn parse_line(line: String) -> ParsedLine {
                         // Binary
                         if line.contains("=") {
                             // Don't capture the character after "=" again.
-                            &temp_comp.push_str( line.chars().skip(pos_of_op).take(2).collect::<String>().as_str());
+                            &temp_comp.push_str(
+                                line.chars()
+                                    .skip(pos_of_op)
+                                    .take(2)
+                                    .collect::<String>()
+                                    .as_str(),
+                            );
                         } else {
-                            &temp_comp.push_str(line.chars().skip(pos_of_op - 1).take(3).collect::<String>().as_str());
+                            &temp_comp.push_str(
+                                line.chars()
+                                    .skip(pos_of_op - 1)
+                                    .take(3)
+                                    .collect::<String>()
+                                    .as_str(),
+                            );
                         }
                     }
                 } else if pos_of_op == 0 {
@@ -138,20 +161,20 @@ fn parse_line(line: String) -> ParsedLine {
                 if line.contains("=") {
                     // We'll have parsed the destination and the comp already; just take care of jump.
                     let i = line.chars().position(|x| x == ';').unwrap();
-                    jmp = Some(line.chars().skip(i+1).take(3).collect::<String>());
+                    jmp = Some(line.chars().skip(i + 1).take(3).collect::<String>());
                 } else {
                     // There is no destination, so we may or may not have parsed the comp.
                     if line.contains(|x| x == '+' || x == '-' || x == '&' || x == '|') {
                         // We'll have parsed the operation and operator. Just parse the Jump.
                         let i = line.chars().position(|x| x == ';').unwrap();
-                        jmp = Some(line.chars().skip(i+1).take(3).collect::<String>());
+                        jmp = Some(line.chars().skip(i + 1).take(3).collect::<String>());
                     } else {
                         // There's no destination, and no operation; comp is just a register or memory.
                         // In this case, we need to store the comparison register as well as process the jump.
                         let i = line.chars().position(|x| x == ';').unwrap();
                         let com = line.chars().nth(i - 1).unwrap();
                         temp_comp.push(com);
-                        jmp = Some(line.chars().skip(i+1).take(3).collect::<String>());
+                        jmp = Some(line.chars().skip(i + 1).take(3).collect::<String>());
                     }
                 }
             }
@@ -160,7 +183,13 @@ fn parse_line(line: String) -> ParsedLine {
     if !temp_comp.is_empty() {
         com = Some(temp_comp.to_string());
     }
-    ParsedLine {command_type: ct, symbol: sym, dest: des, comp: com, jump: jmp}
+    ParsedLine {
+        command_type: ct,
+        symbol: sym,
+        dest: des,
+        comp: com,
+        jump: jmp,
+    }
 }
 
 /// Return the contents of the supplied file as a String, or panic.
@@ -195,7 +224,10 @@ fn preprocess_line(line: String) -> Option<String> {
             None
         } else {
             let comment_start_index = line_trimmed.chars().position(|x| x == '/').unwrap();
-            let line_nocomment = line_trimmed.chars().take(comment_start_index - 1).collect::<String>();
+            let line_nocomment = line_trimmed
+                .chars()
+                .take(comment_start_index - 1)
+                .collect::<String>();
             Some(line_nocomment.split_whitespace().collect::<String>())
         }
     } else {
@@ -208,12 +240,104 @@ fn preprocess_line(line: String) -> Option<String> {
     }
 }
 
-fn setup_for_writing(translated_line: String) {
-    unimplemented!();
-}
-
 fn translate(instruction: ParsedLine) -> String {
-    todo!();
+    /* Translate the parsed content into their corrosponding binary instructions.
+    Each piece of ParsedLine (except LCommands, which are special) has one and only one binary representation.
+    A instructions are just translated into the binary representation of their symbol, with a leading 0 eg:
+        @2 --> 0000000000000010
+    LCommands have a non-number value as their symbol and require consulting a symbol table we populate at some point.
+        @dog --> | dog | 16 | --> 0000000000010000
+    C instructions have multiple parts, one per field with three leading 1s:
+        D=A+1;JMP ->  111accccccdddjjj where acccccc are determined by the comp, ddd by dest, and jjj by jump.
+        ddd and jjj are 1->1 mapping of code -> bits, but
+        acccccc can have different comps lead to the same binary value with the only difference being the a-bit
+        However, we should still be able to store each individual comp as a key in a hash map between binary values:
+        !M -> 1110001
+        !A -> 0110001
+        which are examples of instructions that differ only in the a-bit.
+    */
+    // Rust doesn't have a hash literal, so we have to collect them from something else. These don't work with `const`
+    // because they use non-const functions, so here they are.
+    let dest_map: HashMap<Option<String>, &str> = [
+        (None, "000"),
+        (Some("M".to_string()), "001"),
+        (Some("D".to_string()), "010"),
+        (Some("MD".to_string()), "011"),
+        (Some("A".to_string()), "100"),
+        (Some("AM".to_string()), "101"),
+        (Some("AD".to_string()), "110"),
+        (Some("AMD".to_string()), "111"),
+    ]
+    .iter()
+    .cloned()
+    .collect();
+
+    let jump_map: HashMap<Option<String>, &str> = [
+        (None, "000"),
+        (Some("JGT".to_string()), "001"),
+        (Some("JEQ".to_string()), "010"),
+        (Some("JGE".to_string()), "011"),
+        (Some("JLT".to_string()), "100"),
+        (Some("JNE".to_string()), "101"),
+        (Some("JLE".to_string()), "110"),
+        (Some("JMP".to_string()), "111"),
+    ]
+    .iter()
+    .cloned()
+    .collect();
+
+    let comp_map: HashMap<Option<String>, &str> = [
+        (Some("0".to_string()), "0101010"),
+        (Some("1".to_string()), "0111111"),
+        (Some("-1".to_string()), "0111010"),
+        (Some("D".to_string()), "0001100"),
+        (Some("A".to_string()), "0110000"),
+        (Some("M".to_string()), "1110000"),
+        (Some("!D".to_string()), "0001101"),
+        (Some("!A".to_string()), "0110001"),
+        (Some("!M".to_string()), "1110001"),
+        (Some("-D".to_string()), "0001111"),
+        (Some("-A".to_string()), "0110011"),
+        (Some("-M".to_string()), "1110011"),
+        (Some("D+1".to_string()), "0011111"),
+        (Some("A+1".to_string()), "0110111"),
+        (Some("M+1".to_string()), "1110111"),
+        (Some("D-1".to_string()), "0001110"),
+        (Some("A-1".to_string()), "0110010"),
+        (Some("M-1".to_string()), "1110010"),
+        (Some("D+A".to_string()), "0000010"),
+        (Some("D+M".to_string()), "1000010"),
+        (Some("D-A".to_string()), "0010011"),
+        (Some("D-M".to_string()), "1010011"),
+        (Some("A-D".to_string()), "0000111"),
+        (Some("M-D".to_string()), "1000111"),
+        (Some("D&A".to_string()), "0000000"),
+        (Some("D&M".to_string()), "1000000"),
+        (Some("D|A".to_string()), "0010101"),
+        (Some("D|M".to_string()), "1010101"),
+    ]
+    .iter()
+    .cloned()
+    .collect();
+
+    if instruction.command_type == CommandKind::ACommand {
+        let addr_as_binstr = format!("{:b}", instruction.symbol.unwrap().parse::<u16>().unwrap());
+        let mut final_addr_binstr = addr_as_binstr.to_owned();
+        // Pad the ouput with enough zeros to "become" a 16-bit word.
+        for _ in 0..(16 - addr_as_binstr.len()) {
+            final_addr_binstr.insert(0, '0');
+        }
+        final_addr_binstr
+    } else if instruction.command_type == CommandKind::LCommand {
+        // Will handle later. Just have to impliment a symbol table.
+        todo!()
+    } else {
+        // We're a C instruction. The word is 111accccccdddjjj:
+        let comp_bits = comp_map.get(&instruction.comp).unwrap();
+        let dest_bits = dest_map.get(&instruction.dest).unwrap();
+        let jump_bits = jump_map.get(&instruction.jump).unwrap();
+        "111".to_string() + comp_bits + dest_bits + jump_bits
+    }
 }
 
 /// Write the supplied string to the file "filename".
@@ -236,24 +360,35 @@ fn main() {
         Some(filename) => String::from(filename.to_str().unwrap()),
         None => panic!("We tried to get the filename from user's input, but one didn't exist!"),
     };
-    output_filename.push_str(".hack");
+    output_filename.push_str("-assembled.hack");
 
-    match write_binary_to_file(output_filename, contents.to_owned()) {
-        Ok(_) => (),
-        Err(e) => panic!("Failed to write output to file: {}", e),
-    };
-
+    let mut translated_contents = String::new();
     for line in contents.lines() {
         // parsed_line is either a valid instruction or the ICommand, which we just do nothing with.
         let parsed_line = match preprocess_line(line.to_string()) {
-                Some(instr) => parse_line(instr),
-                None => ParsedLine {command_type: CommandKind::ICommand, dest: None, symbol: None, comp: None, jump: None},
+            Some(instr) => parse_line(instr),
+            None => ParsedLine {
+                command_type: CommandKind::ICommand,
+                dest: None,
+                symbol: None,
+                comp: None,
+                jump: None,
+            },
         };
-       if parsed_line.command_type != CommandKind::ICommand {
+        if parsed_line.command_type != CommandKind::ICommand {
             // We've got a valid instruction, so translate it and send it to the buffer to be written.
             // This "buffer" might be a long String with a bunch of \n in it, or it may actually be a WriteBuf
             // setup_for_writing(translate(parsed_line));
-            println!("{:?}", &parsed_line);
+            translated_contents = if translated_contents.is_empty() {
+                translated_contents + translate(parsed_line).as_str()
+            } else {
+                translated_contents + "\n" + translate(parsed_line).as_str()
+            }
         }
     }
+
+    match write_binary_to_file(output_filename, translated_contents.to_owned()) {
+        Ok(_) => (),
+        Err(e) => panic!("Failed to write output to file: {}", e),
+    };
 }
